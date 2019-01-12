626cec0eaae307ffbb052fbb627a2b466262200c
kasakaid <kasakaid@outlook.com>
2019-01-13 07:30:39 +0900

    we change java version from 8 to 11

--- src/test/java/com/kasakaid/jpaandquerydsl/spring/MyResource.java
+++ src/test/java/com/kasakaid/jpaandquerydsl/spring/MyResource.java
@@ -12,12 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.io.File;
 import java.sql.SQLException;
 
-@TestComponent
+@Component
 @Slf4j
 public class MyResource extends ExternalResource {
 
