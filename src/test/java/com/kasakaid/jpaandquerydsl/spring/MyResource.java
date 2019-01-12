1c6ee5c2ad89f87ee4cc980c407f1e02628f00d9
kasakaid <kasakaid@outlook.com>
2019-01-13 01:44:22 +0900

    merge master...

--- src/test/java/com/kasakaid/jpaandquerydsl/spring/MyResource.java
+++ src/test/java/com/kasakaid/jpaandquerydsl/spring/MyResource.java
@@ -9,7 +9,6 @@ import org.dbunit.ext.mysql.MySqlDataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.rules.ExternalResource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
